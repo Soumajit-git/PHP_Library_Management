88c8e93f985f6c141cb8363f9f0f1c0bd5208027
Soumajit Banerjee <soumajit12a40@gmail.com>
2025-08-02 21:53:15 +0530

    Update footer.php

--- library/admin/includes/footer.php
+++ library/admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                 &copy 2025 Online Library Management System 
+                 &copy 2025 Online Library Management System | Admin Panel
                 </div>
 
             </div>
